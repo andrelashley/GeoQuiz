515b295f07a89f182ddaf5942c9eee39feb99a6f
André Lashley <andre.lashley@gmail.com>
2020-04-18 15:53:10 -0700

    Modify position of toasts in main activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -18,21 +19,26 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener{ view: View ->
-            Toast.makeText(
+        trueButton.setOnClickListener { view: View ->
+            val t = Toast.makeText(
                 this,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+                Toast.LENGTH_SHORT
+            )
+
+            t.setGravity(Gravity.TOP, 0, 0)
+            t.show()
         }
 
-        falseButton.setOnClickListener{ view: View ->
-            Toast.makeText(
+        falseButton.setOnClickListener { view: View ->
+            val t = Toast.makeText(
                 this,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+                Toast.LENGTH_SHORT
+            )
 
+            t.setGravity(Gravity.TOP, 0, 0)
+            t.show()
         }
 
     }
