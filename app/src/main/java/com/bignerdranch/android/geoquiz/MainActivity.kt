7c4e9a226ea8bed68acaef238eec95ef0890d8c7
André Lashley <andre.lashley@gmail.com>
2020-04-18 20:33:34 -0700

    Complete MVC chapter challenges

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -20,7 +21,8 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_mideast, false),
         Question(R.string.question_africa, false),
         Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
+        Question(R.string.question_asia, true)
+    )
 
     private var currentIndex = 0
 
@@ -31,17 +33,29 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
-        trueButton.setOnClickListener{ view: View ->
+        trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
         }
 
-        falseButton.setOnClickListener{ view: View ->
+        falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
         }
 
-        nextButton.setOnClickListener{
+        questionTextView.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener { view: View ->
+            if (currentIndex == 0) return@setOnClickListener
+            currentIndex -= 1
+            updateQuestion()
+        }
+
+        nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
